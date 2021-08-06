cdbb12bacf9d6dcd3967c91ee1fc338db41f7663
Felipe Carasco <felipe.carasco@nudgedigital.co.uk>
2021-08-06 18:17:54 +0100

    Improve load cash files and import transaction

---
+++ HousingFinanceInterimApi/V1/UseCase/Interfaces/ILoadTransactionsUseCase.cs
@@ -0,0 +1,12 @@
+using HousingFinanceInterimApi.V1.Domain;
+using System.Threading.Tasks;
+
+namespace HousingFinanceInterimApi.V1.UseCase.Interfaces
+{
+    public interface ILoadTransactionsUseCase
+    {
+        public Task<bool> ExecuteAsync();
+
+    }
+
+}
