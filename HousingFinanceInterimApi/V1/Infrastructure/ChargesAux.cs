295d0bd5ba4fe233a38ad08dfdfead7871d2ebc4
Felipe Carasco <felipe.carasco@nudgedigital.co.uk>
2021-09-22 10:09:28 +0100

    Create process to load charges spreadsheet

---
+++ HousingFinanceInterimApi/V1/Infrastructure/ChargesAux.cs
@@ -0,0 +1,120 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HousingFinanceInterimApi.V1.Infrastructure
+{
+    [Table("ChargesAux")]
+    public class ChargesAux
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public string TenancyAgreementRef { get; set; }
+
+        public string PropertyRef { get; set; }
+
+        public string ChargePeriod { get; set; }
+
+        public decimal DAT { get; set; }
+
+        public decimal DBR { get; set; }
+
+        public decimal DC4 { get; set; }
+
+        public decimal DC5 { get; set; }
+
+        public decimal DCB { get; set; }
+
+        public decimal DCC { get; set; }
+
+        public decimal DCE { get; set; }
+
+        public decimal DCI { get; set; }
+
+        public decimal DCO { get; set; }
+
+        public decimal DCP { get; set; }
+
+        public decimal DCT { get; set; }
+
+        public decimal DGA { get; set; }
+
+        public decimal DGM { get; set; }
+
+        public decimal DGR { get; set; }
+
+        public decimal DHA { get; set; }
+
+        public decimal DHE { get; set; }
+
+        public decimal DHM { get; set; }
+
+        public decimal DIN { get; set; }
+
+        public decimal DIT { get; set; }
+
+        public decimal DKF { get; set; }
+
+        public decimal DLL { get; set; }
+
+        public decimal DLP { get; set; }
+
+        public decimal DMC { get; set; }
+
+        public decimal DMJ { get; set; }
+
+        public decimal DMR { get; set; }
+
+        public decimal DR5 { get; set; }
+
+        public decimal DRP { get; set; }
+
+        public decimal DRR { get; set; }
+
+        public decimal DSA { get; set; }
+
+        public decimal DSB { get; set; }
+
+        public decimal DSC { get; set; }
+
+        public decimal DSJ { get; set; }
+
+        public decimal DSO { get; set; }
+
+        public decimal DSR { get; set; }
+
+        public decimal DST { get; set; }
+
+        public decimal DTA { get; set; }
+
+        public decimal DTC { get; set; }
+
+        public decimal DTL { get; set; }
+
+        public decimal DTV { get; set; }
+
+        public decimal DVA { get; set; }
+
+        public decimal DWR { get; set; }
+
+        public decimal DWS { get; set; }
+
+        public decimal DWW { get; set; }
+
+        public decimal RCI { get; set; }
+
+        public decimal RPD { get; set; }
+
+        public decimal RSJ { get; set; }
+
+        public decimal RTM { get; set; }
+
+        public decimal RWA { get; set; }
+
+        public decimal WON { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTimeOffset TimeStamp { get; set; }
+    }
+}
---
+++ HousingFinanceInterimApi/V1/UseCase/LoadChargesUseCase.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HousingFinanceInterimApi.V1.Domain;
+using HousingFinanceInterimApi.V1.Factories;
+using HousingFinanceInterimApi.V1.Gateways.Interface;
+using HousingFinanceInterimApi.V1.UseCase.Interfaces;
+using System.Threading.Tasks;
+using Google.Apis.Drive.v3.Data;
+using HousingFinanceInterimApi.V1.Boundary.Response;
+using HousingFinanceInterimApi.V1.Handlers;
+using HousingFinanceInterimApi.V1.Infrastructure;
+
+namespace HousingFinanceInterimApi.V1.UseCase
+{
+    public class LoadChargesUseCase : ILoadChargesUseCase
+    {
+        private readonly IBatchLogGateway _batchLogGateway;
+        private readonly IBatchLogErrorGateway _batchLogErrorGateway;
+        private readonly IChargesGateway _chargesGateway;
+        private readonly IGoogleFileSettingGateway _googleFileSettingGateway;
+        private readonly IGoogleClientService _googleClientService;
+
+        private readonly string _waitDuration = Environment.GetEnvironmentVariable("WAIT_DURATION");
+
+        private const string ChargesLabel = "Charges";
+
+        public LoadChargesUseCase(
+            IBatchLogGateway batchLogGateway,
+            IBatchLogErrorGateway batchLogErrorGateway,
+            IChargesGateway chargesGateway,
+            IGoogleFileSettingGateway googleFileSettingGateway,
+            IGoogleClientService googleClientService)
+        {
+            _batchLogGateway = batchLogGateway;
+            _batchLogErrorGateway = batchLogErrorGateway;
+            _chargesGateway = chargesGateway;
+            _googleFileSettingGateway = googleFileSettingGateway;
+            _googleClientService = googleClientService;
+        }
+
+        public async Task<StepResponse> ExecuteAsync()
+        {
+            LoggingHandler.LogInfo($"Starting charges import");
+
+            var existBatchToday = await _batchLogGateway.GetAsync(ChargesLabel).ConfigureAwait(false);
+            if (existBatchToday != null)
+            {
+                LoggingHandler.LogInfo($"Exists a charges load process today");
+                return new StepResponse() { Continue = false, NextStepTime = DateTime.Now.AddSeconds(0) };
+            }
+
+            const string sheetName = "Active";
+            const string sheetRange = "A:AZ";
+
+            var batch = await _batchLogGateway.CreateAsync(ChargesLabel).ConfigureAwait(false);
+            var googleFileSettings = await GetGoogleFileSetting(ChargesLabel).ConfigureAwait(false);
+
+            if (googleFileSettings == null)
+                return new StepResponse() { Continue = false, NextStepTime = DateTime.Now.AddSeconds(int.Parse(_waitDuration)) };
+
+            var chargesAux = await _googleClientService
+                .ReadSheetToEntitiesAsync<ChargesAuxDomain>(googleFileSettings.GoogleIdentifier, sheetName, sheetRange)
+                .ConfigureAwait(false);
+
+            if (!chargesAux.Any())
+            {
+                LoggingHandler.LogInfo($"No charges data to import");
+                return new StepResponse() { Continue = false, NextStepTime = DateTime.Now.AddSeconds(int.Parse(_waitDuration)) };
+            }
+
+            await HandleSpreadSheet(batch.Id, chargesAux).ConfigureAwait(false);
+
+            await _batchLogGateway.SetToSuccessAsync(batch.Id).ConfigureAwait(false);
+            LoggingHandler.LogInfo($"End charges import");
+            return new StepResponse() { Continue = true, NextStepTime = DateTime.Now.AddSeconds(int.Parse(_waitDuration)) };
+        }
+
+        private async Task<GoogleFileSettingDomain> GetGoogleFileSetting(string label)
+        {
+            LoggingHandler.LogInfo($"Getting Google file setting for '{label}' label");
+            var googleFileSettings = await _googleFileSettingGateway.GetSettingsByLabel(label).ConfigureAwait(false);
+            LoggingHandler.LogInfo($"{googleFileSettings.Count} Google file settings found");
+
+            return googleFileSettings.FirstOrDefault();
+        }
+
+        private async Task HandleSpreadSheet(long batchId, IList<ChargesAuxDomain> chargesAux)
+        {
+            try
+            {
+                LoggingHandler.LogInfo($"Clear aux table");
+                await _chargesGateway.ClearChargesAuxiliary().ConfigureAwait(false);
+
+                LoggingHandler.LogInfo($"Starting bulk insert");
+                await _chargesGateway.CreateBulkAsync(chargesAux).ConfigureAwait(false);
+
+                LoggingHandler.LogInfo($"Starting merge charges");
+                await _chargesGateway.LoadCharges().ConfigureAwait(false);
+
+                LoggingHandler.LogInfo("File success");
+            }
+            catch (Exception exc)
+            {
+                var namespaceLabel = $"{nameof(HousingFinanceInterimApi)}.{nameof(Handler)}.{nameof(HandleSpreadSheet)}";
+
+                await _batchLogErrorGateway.CreateAsync(batchId, ChargesLabel, $"Application error. Not possible to load charges").ConfigureAwait(false);
+
+                LoggingHandler.LogError($"{namespaceLabel} Application error");
+                LoggingHandler.LogError(exc.ToString());
+
+                throw;
+            }
+        }
+    }
+}
