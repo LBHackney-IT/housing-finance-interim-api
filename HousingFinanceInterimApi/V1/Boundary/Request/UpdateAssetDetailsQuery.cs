b52455dcc379f003c8165a8d45bf98dbd2621f2e
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-05-25 14:34:49 +0100

    Add Patch asset endpoint

---
+++ HousingFinanceInterimApi/V1/Boundary/Request/UpdateAssetDetailsQuery.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HousingFinanceInterimApi.V1.Boundary.Request
+{
+    public class UpdateAssetDetailsQuery
+    {
+        [FromRoute(Name = "properyReference")]
+        public string PropertyReference { get; set; }
+    }
+}
