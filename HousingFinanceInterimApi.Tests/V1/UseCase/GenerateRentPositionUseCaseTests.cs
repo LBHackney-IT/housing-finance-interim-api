7fb87f2c6a3c292eda1953a2133230ccd79306ae
Adam <adam.tracy20@gmail.com>
2023-08-21 10:03:02 +0100

    Pinned dates in tests for consistency

--- HousingFinanceInterimApi.Tests/V1/UseCase/GenerateRentPositionUseCaseTests.cs
+++ HousingFinanceInterimApi.Tests/V1/UseCase/GenerateRentPositionUseCaseTests.cs
@@ -11,6 +11,7 @@ using FluentAssertions;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
+using Bogus.DataSets;
 using Google.Apis.Drive.v3.Data;
 
 namespace HousingFinanceInterimApi.Tests.V1.UseCase
@@ -144,7 +145,7 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             var fileList = RandomGen.CreateMany<File>(quantity: 2).ToList();
             var fileToBeDeleted = fileList.First();
             var fileToNotBeDeleted = fileList.Last();
-            fileToBeDeleted.CreatedTime = DateTime.Today.AddDays(-10);
+            fileToBeDeleted.CreatedTime = new DateTime(2020, 1, 1);
             fileToNotBeDeleted.CreatedTime = DateTime.Today.AddDays(-2);
 
             _mockBatchLogGateway
@@ -205,7 +206,7 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             var fileList = RandomGen.CreateMany<File>(quantity: 2).ToList();
             var fileToBeDeleted = fileList.First();
             var fileToNotBeDeleted = fileList.Last();
-            fileToBeDeleted.CreatedTime = DateTime.Today.AddDays(-10);
+            fileToBeDeleted.CreatedTime = new DateTime(2020, 6, 4);
             fileToNotBeDeleted.CreatedTime = DateTime.Today.AddDays(-2);
 
             _mockBatchLogGateway
@@ -313,6 +314,7 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
         [Fact]
         public async Task DoesNotDeleteFilesCreatedAtTheEndOfPreviousFinancialYears()
         {
+            // Does not delete files created on 31st March
             // Arrange
             var rentPositionFileSettings = RandomGen.CreateMany<GoogleFileSettingDomain>(quantity: 1).ToList();
             var rentPositionBkpFileSettings = RandomGen.CreateMany<GoogleFileSettingDomain>(quantity: 1).ToList();
@@ -322,8 +324,8 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             var fileToBePreserved = fileList.First();
             var fileToBeDeleted = fileList.Last();
 
-            fileToBePreserved.CreatedTime = new DateTime(2020, 3, 31);
-            fileToBeDeleted.CreatedTime = DateTime.Today.AddDays(-10);
+            fileToBePreserved.CreatedTime = new DateTime(2020, 3, 31); // Tuesday
+            fileToBeDeleted.CreatedTime = new DateTime(2020, 4, 1);
 
             _mockBatchLogGateway
                 .Setup(g => g.CreateAsync(It.Is<string>(s => s == rentPosition), It.IsAny<bool>()))
@@ -381,8 +383,8 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             var fileToBePreserved = fileList.First();
             var fileToBeDeleted = fileList.Last();
 
-            fileToBePreserved.CreatedTime = new DateTime(2019, 3, 29);
-            fileToBeDeleted.CreatedTime = new DateTime(2019, 3, 31);
+            fileToBePreserved.CreatedTime = new DateTime(2019, 3, 29); // Friday
+            fileToBeDeleted.CreatedTime = new DateTime(2019, 3, 31); // Sunday
 
             _mockBatchLogGateway
                 .Setup(g => g.CreateAsync(It.Is<string>(s => s == rentPosition), It.IsAny<bool>()))
@@ -437,9 +439,12 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             var rentPosition = ConstantsGen.RentPositionLabel;
             var rentPositionBkp = ConstantsGen.RentPositionBkpLabel;
             var fileList = RandomGen.CreateMany<File>(quantity: 3).ToList();
-            fileList.First().CreatedTime = DateTime.Today.AddDays(-2);
-            fileList[1].CreatedTime = DateTime.Today.AddDays(-9); // File to be deleted
-            fileList.Last().CreatedTime = DateTime.Today.AddDays(-10); // File to be deleted
+            var fileToBePreserved = fileList.First();
+            var fileToBeDeleted1 = fileList[1];
+            var fileToBeDeleted2 = fileList.Last();
+            fileToBePreserved.CreatedTime = DateTime.Today.AddDays(-2);
+            fileToBeDeleted1.CreatedTime = new DateTime(2023, 1, 11);
+            fileToBeDeleted2.CreatedTime = new DateTime(2022, 6, 13);
 
             _mockBatchLogGateway
                 .Setup(g => g.CreateAsync(It.Is<string>(s => s == rentPosition), It.IsAny<bool>()))
@@ -483,7 +488,8 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
 
             // Assert
             await useCaseCall.Should().ThrowAsync<AggregateException>().ConfigureAwait(false);
-            _mockGoogleClientService.Verify(x => x.DeleteFileInDrive(It.IsAny<string>()), Times.Exactly(2));
+            _mockGoogleClientService.Verify(x => x.DeleteFileInDrive(It.IsIn(fileToBeDeleted1.Id, fileToBeDeleted2.Id)), Times.Exactly(2));
+            _mockGoogleClientService.Verify(x => x.DeleteFileInDrive(It.Is<string>(s => s == fileToBePreserved.Id)), Times.Never);
             _mockGoogleClientService.Verify(x => x.UploadCsvFile(It.IsAny<List<string[]>>(), It.IsAny<string>(), rentPositionFileSettings.First().GoogleIdentifier), Times.Once);
         }
     }
