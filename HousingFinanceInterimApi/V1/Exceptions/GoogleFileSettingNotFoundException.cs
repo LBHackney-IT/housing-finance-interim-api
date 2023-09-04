1f6d69e0186355d6e015f4dba32e121593d70533
Adam <adam.tracy20@gmail.com>
2023-09-04 16:28:22 +0100

    Dotnet format

--- HousingFinanceInterimApi/V1/Exceptions/GoogleFileSettingNotFoundException.cs
+++ HousingFinanceInterimApi/V1/Exceptions/GoogleFileSettingNotFoundException.cs
@@ -1,7 +1,7 @@
 using System;
 namespace HousingFinanceInterimApi.Tests.V1.UseCase.Exceptions;
 
-public class GoogleFileSettingNotFoundException: Exception
+public class GoogleFileSettingNotFoundException : Exception
 {
     public GoogleFileSettingNotFoundException(string googleFileSettingLabel)
         : base($"No Google File Settings found with label: {googleFileSettingLabel}.")
