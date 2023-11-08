cecb92e9847990b81d4e7b9c13560d59da1b0527
AtiyyahRawat <atiyyah.rawat@gmail.com>
2023-11-08 15:02:19 +0000

    RefreshCurrentBalance tests

--- HousingFinanceInterimApi/V1/UseCase/RefreshCurrentBalanceUseCase.cs
+++ HousingFinanceInterimApi/V1/UseCase/RefreshCurrentBalanceUseCase.cs
@@ -1,10 +1,6 @@
 using System;
-using AutoMapper;
-using HousingFinanceInterimApi.V1.Domain;
-using HousingFinanceInterimApi.V1.Factories;
 using HousingFinanceInterimApi.V1.Gateways.Interface;
 using HousingFinanceInterimApi.V1.UseCase.Interfaces;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using HousingFinanceInterimApi.V1.Boundary.Response;
 using HousingFinanceInterimApi.V1.Handlers;
