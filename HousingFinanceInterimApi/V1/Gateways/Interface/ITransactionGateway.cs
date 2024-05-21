3d84594da4a08c77b6d0b168c71e86b8550a5e17
duslerke <43747286+Duslerke@users.noreply.github.com>
2024-05-21 14:34:29 +0100

    Add PRN Transactions retrieval method to the Transactions Gateway.

--- HousingFinanceInterimApi/V1/Gateways/Interface/ITransactionGateway.cs
+++ HousingFinanceInterimApi/V1/Gateways/Interface/ITransactionGateway.cs
@@ -2,12 +2,15 @@ using System;
 using HousingFinanceInterimApi.V1.Infrastructure;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using HousingFinanceInterimApi.V1.Domain;
+using HousingFinanceInterimApi.V1.Domain.ArgumentWrappers;
 
 namespace HousingFinanceInterimApi.V1.Gateways.Interface
 {
     public interface ITransactionGateway
     {
         public Task<IList<Transaction>> ListAsync(DateTime? startDate, DateTime? endDate);
+        public Task<IList<PRNTransactionDomain>> GetPRNTransactions(GetPRNTransactionsDomain filterArguments);
 
         public Task LoadCashFilesTransactions();
 
