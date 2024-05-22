2914039f14f4a0bc3d24c3df44f9c11332c894b9
duslerke <43747286+Duslerke@users.noreply.github.com>
2024-05-22 15:47:02 +0100

    Add week/month period integer fields to the BatchReport event model.

--- HousingFinanceInterimApi/V1/Factories/BatchReport.cs
+++ HousingFinanceInterimApi/V1/Factories/BatchReport.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using HousingFinanceInterimApi.V1.Boundary.Request;
 using HousingFinanceInterimApi.V1.Boundary.Response;
 using HousingFinanceInterimApi.V1.Domain;
-using HousingFinanceInterimApi.V1.Infrastructure;
 
 namespace HousingFinanceInterimApi.V1.Factories
 {
@@ -25,6 +24,8 @@ namespace HousingFinanceInterimApi.V1.Factories
                 ReportEndDate = batchReport.ReportEndDate,
                 ReportYear = batchReport.ReportYear,
                 ReportDate = batchReport.ReportDate,
+                ReportStartWeekOrMonth = batchReport.ReportStartWeekOrMonth,
+                ReportEndWeekOrMonth = batchReport.ReportEndWeekOrMonth,
                 Link = batchReport.Link,
                 StartTime = batchReport.StartTime,
                 EndTime = batchReport.EndTime,
@@ -165,6 +166,7 @@ namespace HousingFinanceInterimApi.V1.Factories
             return batchReportHousingBenefitAcademy?.Select(b => b.ToDomain()).ToList();
         }
 
+        // TODO: this change belongs to the POST endpoint PR.
         public static Infrastructure.BatchReport ToDatabase(this BatchReportDomain batchReport)
         {
             if (batchReport == null)
