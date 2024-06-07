32056567d87371ae0d8dc9277e2de3d815935356
duslerke <43747286+Duslerke@users.noreply.github.com>
2024-06-07 15:36:24 +0100

    Move the remaining SQL objects with prefix run order into the execution dir.

--- unit_tests_database/sql_order_control.sh
+++ unit_tests_database/sql_order_control.sh
@@ -8,6 +8,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCRIPT_DIR
 
 
+declare -A object_prefixes
+object_prefixes[tables]="a"
+object_prefixes[functions]="b"
+object_prefixes[views]="c"
+object_prefixes[stored_procedures]="d"
+
 relative_table_order=(
     # batch log group
     batch_log
@@ -41,6 +47,11 @@ for file in "${relative_table_order[@]}"; do
     mv "tables/$file.sql" "tables/${padded_i}_${file}.sql"
 done
 
-for filename in tables/*.sql; do mv {tables/,a_}"$(basename $filename)"; done;
+for filename in tables/*.sql; do mv {tables/,${object_prefixes[tables]}_}"$(basename $filename)"; done;
+for filename in functions/*.sql; do mv {functions/,${object_prefixes[functions]}_}"$(basename $filename)"; done;
+for filename in views/*.sql; do mv {views/,${object_prefixes[views]}_}"$(basename $filename)"; done;
+for filename in stored_procedures/*.sql; do mv {stored_procedures/,${object_prefixes[stored_procedures]}_}"$(basename $filename)"; done;
+
+rm -r tables functions views stored_procedures
 
 rm -r tables
