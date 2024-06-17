c0986ab6081232f380f663d80322938c79af7171
Adam <74552077+adamtry@users.noreply.github.com>
2024-06-17 13:24:09 +0100

    Docker unit test setup for ee (#52)
    * Resolve adjusted charge value test
    
    * Add case for partial adjustments
    
    * Use decimal to match DB
    
    * Float to decimal for CH amount
    
    * Document udf tests better
    
    * Add test for adjustment exclusions
    
    * Format
    
    * Fix
    
    * Use data gen to generate references
    
    * Address comments
    
    * Setup
    
    * Reduce need for dependencies: 'the --chmod option requires BuildKit'
    
    * Fix views
    
    * Allow stop with ctrl+c
    
    * Fix image on arch distro btw. Tweak initilisation to match official example.
    
    * Minor cleanup
    
    * Add gitguardian exclude
    
    * Update gitguardian.yaml
    
    * gg syntax fix
    
    * Try updating version
    
    * Try ignored paths
    
    * Try again ignore database
    
    * Try simpler password
    
    * Remove gitguardian.yaml, not working
    
    * Try different password
    
    ---------
    
    Co-authored-by: duslerke <43747286+Duslerke@users.noreply.github.com>

---
+++ unit_tests_database_ee/db_setup.sh
@@ -0,0 +1,25 @@
+# Create database
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "CREATE DATABASE sow2b"
+
+# Set to default
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d sow2b -Q "EXEC sp_defaultdb 'sa', 'sow2b'"
+
+# Create tables
+for file in /usr/config/tables/*.sql; do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d sow2b -i $file
+done
+
+# Create functions
+for file in /usr/config/functions/*.sql; do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d sow2b -i $file
+done
+
+# Create stored procedures
+for file in /usr/config/stored_procedures/*.sql; do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d sow2b -i $file
+done
+
+# Create views
+for file in /usr/config/views/*.sql; do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d sow2b -i $file
+done
