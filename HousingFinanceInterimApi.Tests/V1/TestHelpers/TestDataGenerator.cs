4b7ba64f1ab04a02a6d0b8a220db5188b36766ee
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2024-07-23 14:12:58 +0100

    Moved a file
    TestDataGenerator is now in TestHelpers ns

--- HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGenerator.cs
+++ HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGenerator.cs
@@ -1,7 +1,7 @@
 using System;
 using AutoFixture;
 
-namespace HousingFinanceInterimApi.Tests.V1.Helper
+namespace HousingFinanceInterimApi.Tests.V1.TestHelpers
 {
     public static class TestDataGenerator
     {
