10c58fdc2209d4eda4bb88933341c0f86e13eaef
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2024-07-23 11:16:10 +0100

    Updated the AssetDetails Gateway
    Removed reference to a store proc
    Created Entity Framework models
    Updated the database context
    Added a new test for writing to the dev database (thanks Adam T!)

--- HousingFinanceInterimApi/V1/Gateways/AssetGateway.cs
+++ HousingFinanceInterimApi/V1/Gateways/AssetGateway.cs
@@ -1,6 +1,8 @@
 using HousingFinanceInterimApi.V1.Boundary.Request;
 using HousingFinanceInterimApi.V1.Gateways.Interface;
+using HousingFinanceInterimApi.V1.Handlers;
 using HousingFinanceInterimApi.V1.Infrastructure;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace HousingFinanceInterimApi.V1.Gateways
@@ -16,7 +18,40 @@ namespace HousingFinanceInterimApi.V1.Gateways
 
         public async Task UpdateAssetDetails(UpdateAssetDetailsQuery query, UpdateAssetDetailsRequest request)
         {
-            await _context.UpdateAssetDetails(query, request).ConfigureAwait(false);
+            await UpdateAssetDetailsTable(query, request).ConfigureAwait(false);
+        }
+
+        private async Task UpdateAssetDetailsTable(UpdateAssetDetailsQuery query, UpdateAssetDetailsRequest request)
+        {
+            try
+            {
+                var maPropertyToUpdate = _context.MAProperty.SingleOrDefault(p => p.PropRef == query.PropertyReference);
+                var uhPropertyToUpdate = _context.UHProperty.SingleOrDefault(p => p.PropRef == query.PropertyReference);
+
+                if (maPropertyToUpdate != null)
+                {
+                    maPropertyToUpdate.ShortAddress = $"{request.PostPreamble} {request.AddressLine1}";
+                    maPropertyToUpdate.Address1 = $"{request.PostPreamble} {request.AddressLine1}";
+                    maPropertyToUpdate.PostPreamble = request.PostPreamble ?? request.AddressLine1;
+
+                    await _context.SaveChangesAsync().ConfigureAwait(false);
+                }
+
+                if (uhPropertyToUpdate != null)
+                {
+                    uhPropertyToUpdate.ShortAddress = $"{request.PostPreamble} {request.AddressLine1}";
+                    uhPropertyToUpdate.Address1 = $"{request.PostPreamble} {request.AddressLine1}";
+                    uhPropertyToUpdate.PostPreamble = request.PostPreamble ?? request.AddressLine1;
+
+                    await _context.SaveChangesAsync().ConfigureAwait(false);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                LoggingHandler.LogError(ex.Message);
+                LoggingHandler.LogError(ex.StackTrace);
+                throw;
+            }
         }
     }
 }
