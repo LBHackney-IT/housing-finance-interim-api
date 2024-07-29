50b81b541eccf0a8d678ad639a1618f5d5a2dd56
Adam <74552077+adamtry@users.noreply.github.com>
2024-07-29 11:31:02 +0100

    Load Cash File Entity Framework Rewrite (#149)
    Tests for this process, rewritten in Entity Framework.
    
    Runs on the EE Docker environment (make unit_db_ee)
    
    See sproc code: https://github.com/LBHackney-IT/HFSDatabaseObjects/blob/880959d860c087cde6f946f16834e8f5b2416c0a/stored_procedures/usp_LoadCashFile.sql
    
    What the stored procedure does is:
    
        Clears UPCashDump records associated with filenames that were unsuccessfully processed
        For the rest of the UPCashDump records that are unread, parses their FullText into various fields
        Inserts the parsed data into UPCashLoad
        Marks the processed UPCashDump records as read
    
    Other changes:
    
        Switch the DatabaseContext tests to use the Dockerised SQL Server database from the HFSDatabaseObjects module added in 
    
    Add HFSDatabaseObjects submodule for unit test database #148
    Added a helper class + methods to clear tables in reverse dependency order automatically. This can become a major headache if it needs to be done manually on each test, especially as the nesting grows larger.
    Fixed some database entities to ensure EF can create them from fixtures without errors.
    Cleared up redundant entries in the docker-compose.yml as part of preparation for using it to support running these tests in future

--- HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGenerator.cs
+++ HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGenerator.cs
@@ -6,6 +6,7 @@ namespace HousingFinanceInterimApi.Tests.V1.TestHelpers
     public static class TestDataGenerator
     {
         private static readonly Fixture _fixture = new();
+        private static readonly Bogus.Faker _faker = new();
 
         /// <summary>
         /// Property Reference known locally as  prop_ref
@@ -17,6 +18,11 @@ namespace HousingFinanceInterimApi.Tests.V1.TestHelpers
             {
                 return string.Concat("9", _fixture.Create<int>().ToString().PadLeft(7, '0').AsSpan(0, 7));
             }
+        }
+
+        public static string RentAccount()
+        {
+            return _faker.Random.Long(0, 9_999_999_999).ToString().PadLeft(10, '0');
         }
     }
 }
---
+++ HousingFinanceInterimApi/V1/Infrastructure/UPCashLoad.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HousingFinanceInterimApi.V1.Infrastructure
+{
+    [Table("UPCashLoad")]
+    public class UPCashLoad
+    {
+        [Key]
+        public long Id { get; set; }
+        public string RentAccount { get; set; }
+        public string PaymentSource { get; set; }
+        public string MethodOfPayment { get; set; }
+        public decimal AmountPaid { get; set; }
+        public DateTime DatePaid { get; set; }
+        public string CivicaCode { get; set; }
+        public DateTimeOffset Timestamp { get; private set; }
+        public bool IsRead { get; set; }
+
+        [ForeignKey(nameof(UPCashDumpId))]
+        public long UPCashDumpId { get; set; }
+        public UPCashDump UpCashDump { get; set; }
+    }
+
+}
