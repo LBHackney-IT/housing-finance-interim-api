c1dfe3a65918f0a07a5f88ea598ccc24bffdeae0
Adam <adam.tracy20@gmail.com>
2024-08-01 10:01:54 +0100

    Cleanup

--- HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGen.cs
+++ HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGen.cs
@@ -2,48 +2,46 @@ using System;
 using AutoFixture;
 using Bogus;
 
-namespace HousingFinanceInterimApi.Tests.V1.TestHelpers
+namespace HousingFinanceInterimApi.Tests.V1.TestHelpers;
+
+
+public static class TestDataGen
 {
-    public static class TestDataGen
+    private static readonly Fixture _fixture = new();
+    private static readonly Faker _faker = new(locale: "en_GB");
+
+    public static string PropRef
     {
-        private static readonly Fixture _fixture = new();
-        private static readonly Bogus.Faker _faker = new Faker(locale: "en_GB");
-
-        /// <summary>
-        /// Property Reference known locally as  prop_ref
-        /// </summary>
-        /// <returns></returns>
-        public static string PropRef
+        get
         {
-            get
-            {
-                return string.Concat("9", _fixture.Create<int>().ToString().PadLeft(7, '0').AsSpan(0, 7));
-            }
+            return string.Concat("9", _fixture.Create<int>().ToString().PadLeft(7, '0').AsSpan(0, 7));
         }
-
-        public static string RentAccount()
-        {
-            return _faker.Random.Long(0, 9_999_999_999).ToString().PadLeft(10, '0');
-        }
-
     }
-    public static class CashDumpTestData
+
+    public static string RentAccount()
     {
-        private static readonly Faker _faker = new Faker(locale: "en_GB");
-
-        public static string PaymentSource() =>
-            _faker.Random.Word().Replace(" ", "").PadRight(10)[..10].ToUpper();
-        public static string AmountPaid() =>
-            _faker.Random.Decimal(0, 1000).ToString().PadLeft(9, '0')[..9];
-        public static string PaymentDate() =>
-            _faker.Date.Past().ToString("dd/MM/yyyy");
-        public static string TransactionType() =>
-            _faker.Random.AlphaNumeric(3).ToUpper();
-        public static string CivicaCode() =>
-            _faker.Random.Number(1, 99).ToString().PadLeft(2, '0');
-        public static string FullTextBuild(string rentAccount, string paymentSource, string amountPaid, string paymentDate, string transactionType, string civicaCode) =>
-            $"{rentAccount}{paymentSource}".PadRight(30) + $"{transactionType}+{amountPaid}{paymentDate}{civicaCode}";
-        public static string FullText() =>
-            FullTextBuild(TestDataGen.RentAccount(), PaymentSource(), AmountPaid(), PaymentDate(), TransactionType(), CivicaCode());
+        return _faker.Random.Long(0, 9_999_999_999).ToString().PadLeft(10, '0');
     }
+
 }
+
+
+public static class CashDumpTestData
+{
+    private static readonly Faker _faker = new (locale: "en_GB");
+    public static string PaymentSource() =>
+        _faker.Random.Word().Replace(" ", "").PadRight(10)[..10].ToUpper();
+    public static string AmountPaid() =>
+        _faker.Random.Decimal(0, 1000).ToString().PadLeft(9, '0')[..9];
+    public static string PaymentDate() =>
+        _faker.Date.Past().ToString("dd/MM/yyyy");
+    public static string TransactionType() =>
+        _faker.Random.AlphaNumeric(3).ToUpper();
+    public static string CivicaCode() =>
+        _faker.Random.Number(1, 99).ToString().PadLeft(2, '0');
+    public static string FullTextBuild(string rentAccount, string paymentSource, string amountPaid, string paymentDate, string transactionType, string civicaCode) =>
+        $"{rentAccount}{paymentSource}".PadRight(30) + $"{transactionType}+{amountPaid}{paymentDate}{civicaCode}";
+    public static string FullText() =>
+        FullTextBuild(TestDataGen.RentAccount(), PaymentSource(), AmountPaid(), PaymentDate(), TransactionType(), CivicaCode());
+}
+
