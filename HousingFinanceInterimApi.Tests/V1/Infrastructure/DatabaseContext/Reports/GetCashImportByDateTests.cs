a5bd8d2668ea2dc437b1844b12ec8506509f4d83
Adam <adam.tracy20@gmail.com>
2024-08-01 11:34:49 +0100

    Remove brackets

--- HousingFinanceInterimApi.Tests/V1/Infrastructure/DatabaseContext/Reports/GetCashImportByDateTests.cs
+++ HousingFinanceInterimApi.Tests/V1/Infrastructure/DatabaseContext/Reports/GetCashImportByDateTests.cs
@@ -110,7 +110,7 @@ public class GetCashImportByDateTests : IClassFixture<BaseContextTest>
         Assert.Single(reportData);
         var reportItem = reportData[0];
 
-        var expectedDateString = (TestDateThisWeek).ToString("dd/MM/yyyy");
+        var expectedDateString = TestDateThisWeek.ToString("dd/MM/yyyy");
         Assert.Equal(expectedDateString, reportItem["Date"]);
 
         var expectedIfsTotal = ssminiList.Sum(x => x.RealValue);
