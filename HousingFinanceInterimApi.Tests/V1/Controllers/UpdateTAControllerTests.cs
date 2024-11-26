e4264bba600263bd8b7dee8e341b45be2ca28f81
humulla <43747248+humulla@users.noreply.github.com>
2024-11-26 15:22:06 +0000

    action comments and more tests added1 !

--- HousingFinanceInterimApi.Tests/V1/Controllers/UpdateTAControllerTests.cs
+++ HousingFinanceInterimApi.Tests/V1/Controllers/UpdateTAControllerTests.cs
@@ -10,29 +10,29 @@ using Xunit;
 
 namespace HousingFinanceInterimApi.Tests.V1.Controllers
 {
-    public class UpdateTAControllerTests
+    public class TenancyAgreementControllerTests
     {
-        private readonly UpdateTAController _classUnderTest;
+        private readonly TenancyAgreementController _classUnderTest;
 
         private readonly Mock<IUpdateTAGateway> _gateway;
         private readonly Fixture _fixture = new Fixture();
 
-        public UpdateTAControllerTests()
+        public TenancyAgreementControllerTests()
         {
             _gateway = new Mock<IUpdateTAGateway>();
 
-            _classUnderTest = new UpdateTAController(_gateway.Object);
+            _classUnderTest = new TenancyAgreementController(_gateway.Object);
         }
 
         [Fact]
         public async Task UpdateTA_WhenValidRequest_ReturnsNoContent()
         {
             // Arrange
-            var query = _fixture.Create<UpdateTAQuery>();
+            var query = _fixture.Create<string>();
             var request = _fixture.Create<UpdateTARequest>();
 
             // Act
-            var response = await _classUnderTest.UpdateTA(query, request)
+            var response = await _classUnderTest.DynamoDbStreamTrigger(query, request)
                 .ConfigureAwait(false);
 
             // Assert
--- HousingFinanceInterimApi/V1/Gateways/UpdateTAGateway.cs
+++ HousingFinanceInterimApi/V1/Gateways/UpdateTAGateway.cs
@@ -2,6 +2,7 @@ using HousingFinanceInterimApi.V1.Boundary.Request;
 using HousingFinanceInterimApi.V1.Gateways.Interface;
 using HousingFinanceInterimApi.V1.Handlers;
 using HousingFinanceInterimApi.V1.Infrastructure;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,30 +17,30 @@ namespace HousingFinanceInterimApi.V1.Gateways
             _context = context;
         }
 
-        public async Task UpdateTADetails(UpdateTAQuery query, UpdateTARequest request)
+        public async Task UpdateTADetails(string tagRef, UpdateTARequest request)
         {
             try
             {
-                var uhTenancyAgreement = _context.UHTenancyAgreement.SingleOrDefault(p => p.PropRef == query.PropertyReference);
-                var maTenancyAgreement = _context.MATenancyAgreement.SingleOrDefault(p => p.PropRef == query.PropertyReference);
+                var uhTenancyAgreement = _context.UHTenancyAgreement.SingleOrDefault(p => p.TenancyAgreementRef == tagRef);
+                var maTenancyAgreement = _context.MATenancyAgreement.SingleOrDefault(p => p.TenancyAgreementRef == tagRef);
 
                 if (uhTenancyAgreement is not null && maTenancyAgreement is not null)
                 {
-                    uhTenancyAgreement.Eot = request.TenureEndDate;
-                    maTenancyAgreement.Eot = request.TenureEndDate;
-                    if (request.TenureEndDate is not null)
+                    uhTenancyAgreement.EndOfTenure = request.TenureEndDate;
+                    maTenancyAgreement.EndOfTenure = request.TenureEndDate;
+                    if (request.TenureEndDate is not null && request.TenureEndDate.Value !> DateTime.Now)
                     {
-                        uhTenancyAgreement.Terminated = true;
-                        uhTenancyAgreement.Present = false;
-                        maTenancyAgreement.Terminated = true;
-                        maTenancyAgreement.Present = false;
+                        uhTenancyAgreement.IsTerminated = true;
+                        uhTenancyAgreement.IsPresent = false;
+                        maTenancyAgreement.IsTerminated = true;
+                        maTenancyAgreement.IsPresent = false;
                     }
                     else
                     {
-                        uhTenancyAgreement.Terminated = false;
-                        uhTenancyAgreement.Present = true;
-                        maTenancyAgreement.Terminated = false;
-                        maTenancyAgreement.Present = true;
+                        uhTenancyAgreement.IsTerminated = false;
+                        uhTenancyAgreement.IsPresent = true;
+                        maTenancyAgreement.IsTerminated = false;
+                        maTenancyAgreement.IsPresent = true;
                     }
                 }
                 await _context.SaveChangesAsync().ConfigureAwait(false);
