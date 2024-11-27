09f122fc2e43376f6f50fe6ad0a63cddf49fef43
humulla <43747248+humulla@users.noreply.github.com>
2024-11-27 14:31:33 +0000

    more pr comments

--- HousingFinanceInterimApi/V1/Gateways/Interface/IUpdateTAGateway.cs
+++ HousingFinanceInterimApi/V1/Gateways/Interface/IUpdateTAGateway.cs
@@ -1,10 +1,11 @@
 using HousingFinanceInterimApi.V1.Boundary.Request;
+using HousingFinanceInterimApi.V1.Domain;
 using System.Threading.Tasks;
 
 namespace HousingFinanceInterimApi.V1.Gateways.Interface
 {
     public interface IUpdateTAGateway
     {
-        Task UpdateTADetails(string tagRef, UpdateTARequest request);
+        Task UpdateTADetails(string tagRef, UpdateTADomain request);
     }
 }
