edc7dc9142874832785240bdb4322fae94e97ff9
humulla <43747248+humulla@users.noreply.github.com>
2024-12-02 12:46:17 +0000

    more logging

--- HousingFinanceInterimApi/V1/UseCase/UpdateTAUseCase.cs
+++ HousingFinanceInterimApi/V1/UseCase/UpdateTAUseCase.cs
@@ -1,6 +1,8 @@
 using HousingFinanceInterimApi.V1.Boundary.Request;
 using HousingFinanceInterimApi.V1.Factories;
 using HousingFinanceInterimApi.V1.Gateways.Interface;
+using HousingFinanceInterimApi.V1.Handlers;
+using HousingFinanceInterimApi.V1.Infrastructure;
 using HousingFinanceInterimApi.V1.UseCase.Interfaces;
 using System;
 using System.Threading.Tasks;
@@ -19,6 +21,7 @@ namespace HousingFinanceInterimApi.V1.UseCase
         public async Task ExecuteAsync(string tagRef, UpdateTARequest request)
         {
             var domain = request.ToDomain();
+            LoggingHandler.LogInfo($"eot value is {domain.TenureEndDate}, present:{domain.IsPresent}, terminated:{domain.IsTerminated}");
             await _gateway.UpdateTADetails(tagRef, domain).ConfigureAwait(false);
         }
 
