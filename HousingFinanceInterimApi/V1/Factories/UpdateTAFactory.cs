183f194f3823427fc6ed3b68340c784d0dcaec79
humulla <43747248+humulla@users.noreply.github.com>
2024-12-05 12:07:16 +0000

    fix test2

--- HousingFinanceInterimApi/V1/Factories/UpdateTAFactory.cs
+++ HousingFinanceInterimApi/V1/Factories/UpdateTAFactory.cs
@@ -15,7 +15,7 @@ namespace HousingFinanceInterimApi.V1.Factories
             bool isPresent;
             var convertToString = request.TenureEndDate.ToString();
             LoggingHandler.LogInfo($"Date string value is {convertToString}");
-            if (request.TenureEndDate == null || request.TenureEndDate > DateTime.UtcNow || convertToString == "1900-01-01T00:00:00.0000000Z")
+            if (request.TenureEndDate == null || request.TenureEndDate > DateTime.UtcNow || convertToString == "01/01/1900 00:00:00")
             {
                 return new UpdateTADomain
                 {
