b01b5db5872d19a8fb29adda9c58c3b4cb1f17f0
humulla <43747248+humulla@users.noreply.github.com>
2024-12-05 12:26:17 +0000

    extra condition for default date

--- HousingFinanceInterimApi/V1/Factories/UpdateTAFactory.cs
+++ HousingFinanceInterimApi/V1/Factories/UpdateTAFactory.cs
@@ -15,7 +15,7 @@ namespace HousingFinanceInterimApi.V1.Factories
             bool isPresent;
             var convertToString = request.TenureEndDate.ToString();
             LoggingHandler.LogInfo($"Date string value is {convertToString}");
-            if (request.TenureEndDate == null || request.TenureEndDate > DateTime.UtcNow || convertToString == "01/01/1900 00:00:00")
+            if (request.TenureEndDate == null || request.TenureEndDate > DateTime.UtcNow || convertToString == "01/01/1900 12:00:00 AM" || convertToString == "01/01/1900 00:00:00")
             {
                 return new UpdateTADomain
                 {
