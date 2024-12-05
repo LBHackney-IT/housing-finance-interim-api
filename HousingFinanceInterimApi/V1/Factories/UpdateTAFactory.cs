68cfc904c9a5589f3c4a93493198b16d51c96dd1
humulla <43747248+humulla@users.noreply.github.com>
2024-12-05 11:54:40 +0000

    add logging

--- HousingFinanceInterimApi/V1/Factories/UpdateTAFactory.cs
+++ HousingFinanceInterimApi/V1/Factories/UpdateTAFactory.cs
@@ -1,5 +1,6 @@
 using HousingFinanceInterimApi.V1.Boundary.Request;
 using HousingFinanceInterimApi.V1.Domain;
+using HousingFinanceInterimApi.V1.Handlers;
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -13,6 +14,7 @@ namespace HousingFinanceInterimApi.V1.Factories
             bool isTerminated;
             bool isPresent;
             var convertToString = request.TenureEndDate.ToString();
+            LoggingHandler.LogInfo($"Date string value is {convertToString}");
             if (request.TenureEndDate == null || request.TenureEndDate > DateTime.UtcNow || convertToString == "1900-01-01T00:00:00.0000000Z")
             {
                 return new UpdateTADomain
