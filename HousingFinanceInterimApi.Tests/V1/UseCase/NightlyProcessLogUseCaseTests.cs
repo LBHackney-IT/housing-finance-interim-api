4afcc8cf652be03a37c207369dc265ec66025232
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2025-05-12 15:35:11 +0100

    Swapped out static logGroups with an Interface

--- HousingFinanceInterimApi.Tests/V1/UseCase/NightlyProcessLogUseCaseTests.cs
+++ HousingFinanceInterimApi.Tests/V1/UseCase/NightlyProcessLogUseCaseTests.cs
@@ -6,6 +6,7 @@ using Amazon.CloudWatchLogs;
 using Amazon.CloudWatchLogs.Model;
 using FluentAssertions;
 using HousingFinanceInterimApi.V1.Gateway.Interfaces;
+using HousingFinanceInterimApi.V1.Helpers;
 using HousingFinanceInterimApi.V1.Infrastructure;
 using HousingFinanceInterimApi.V1.UseCase;
 using Microsoft.EntityFrameworkCore;
@@ -18,12 +19,14 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
     {
         private readonly Mock<IAmazonCloudWatchLogs> _mockCloudWatchLogsClient;
         private readonly Mock<INightlyProcessLogGateway> _mockGateway;
+        private readonly Mock<ILogGroupProvider> _mockProvider;
         private NightlyProcessLogUseCase _nightlyProcessLogUseCase;
 
         public NightlyProcessLogUseCaseTests()
         {
             _mockGateway = new Mock<INightlyProcessLogGateway>();
             _mockCloudWatchLogsClient = new Mock<IAmazonCloudWatchLogs>();
+            _mockProvider = new Mock<ILogGroupProvider>();
         }
 
         [Fact]
@@ -31,7 +34,7 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
         {
             // Arrange
             var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
 
             var queryResults = new List<List<ResultField>> { new List<ResultField> { new ResultField { Field = "Test", Value = "Value" } } };
 
@@ -42,6 +45,10 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             _mockCloudWatchLogsClient
                 .Setup(x => x.GetQueryResultsAsync(It.IsAny<GetQueryResultsRequest>(), default))
                 .ReturnsAsync(new GetQueryResultsResponse { Status = QueryStatus.Complete, Results = queryResults });
+
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
 
             // Act
             var results = await _nightlyProcessLogUseCase.QueryCloudWatchLogs(logGroups[0]).ConfigureAwait(false);
@@ -56,7 +63,7 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
         {
             // Arrange
             var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
 
             _mockCloudWatchLogsClient
                 .Setup(x => x.StartQueryAsync(It.IsAny<StartQueryRequest>(), default))
@@ -65,6 +72,10 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             _mockCloudWatchLogsClient
                 .Setup(x => x.GetQueryResultsAsync(It.IsAny<GetQueryResultsRequest>(), default))
                 .ReturnsAsync(new GetQueryResultsResponse { Status = QueryStatus.Failed });
+
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
 
             // Act
             Func<Task> act = async () => await _nightlyProcessLogUseCase.QueryCloudWatchLogs(logGroups[0]).ConfigureAwait(false);
@@ -77,11 +88,6 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
         public async Task ExecuteAsync_Should_Process_LogGroups_InSequence()
         {
             // Arrange
-            var logGroups = new List<string> { "/aws/lambda/log-group-function1", "/aws/lambda/log-group-function2" };
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
-
-            var queryResults = new List<List<ResultField>> { new List<ResultField> { new ResultField { Field = "Test", Value = "Value" } } };
-
             _mockGateway
                 .Setup(x => x.UpdateDatabaseWithResults(It.IsAny<string>(), It.IsAny<List<List<ResultField>>>()))
                 .Returns(Task.CompletedTask);
@@ -90,9 +96,17 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
                 .Setup(x => x.StartQueryAsync(It.IsAny<StartQueryRequest>(), default))
                 .ReturnsAsync(new StartQueryResponse { QueryId = "test-query-id" });
 
+            var queryResults = new List<List<ResultField>> { new List<ResultField> { new ResultField { Field = "Test", Value = "Value" } } };
             _mockCloudWatchLogsClient
                 .Setup(x => x.GetQueryResultsAsync(It.IsAny<GetQueryResultsRequest>(), default))
                 .ReturnsAsync(new GetQueryResultsResponse { Status = QueryStatus.Complete, Results = queryResults });
+
+            var logGroups = new List<string> { "/aws/lambda/log-group-function1", "/aws/lambda/log-group-function2" };
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
+
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
 
             // Act
             var response = await _nightlyProcessLogUseCase.ExecuteAsync().ConfigureAwait(false);
@@ -109,9 +123,6 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
         public async Task ExecuteAsync_Should_Log_Any_Errors_And_Continues_Processing()
         {
             // Arrange
-            var logGroups = new List<string> { "log-group-1", "log-group-2" };
-            var useCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
-
             _mockCloudWatchLogsClient
                 .SetupSequence(x => x.StartQueryAsync(It.IsAny<StartQueryRequest>(), default))
                 .ReturnsAsync(new StartQueryResponse { QueryId = "query-id-1" })
@@ -132,8 +143,15 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
                 .Setup(x => x.UpdateDatabaseWithResults(It.IsAny<string>(), It.IsAny<List<List<ResultField>>>()))
                 .Returns(Task.CompletedTask);
 
+            var logGroups = new List<string> { "log-group-1", "log-group-2" };
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
+
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
+
             // Act
-            var response = await useCase.ExecuteAsync();
+            var response = await _nightlyProcessLogUseCase.ExecuteAsync().ConfigureAwait(false);
 
             // Assert
             Assert.NotNull(response);
@@ -152,7 +170,11 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             // Arrange
             var createdDate = default(DateTime);
             var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
+
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
 
             // Act & Assert
             await Assert.ThrowsAsync<ArgumentException>(() => _nightlyProcessLogUseCase.ExecuteAsync(createdDate)).ConfigureAwait(false);
@@ -163,12 +185,16 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
         {
             // Arrange
             var createdDate = DateTime.UtcNow;
-            var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
-
             _mockGateway
                 .Setup(x => x.GetByDateCreatedAsync(createdDate))
                 .ReturnsAsync(new List<NightlyProcessLog>());
+
+            var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
+
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
 
             // Act
             var result = await _nightlyProcessLogUseCase.ExecuteAsync(createdDate).ConfigureAwait(false);
@@ -184,7 +210,6 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
         {
             // Arrange
             var createdDate = DateTime.UtcNow.Date;
-            var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
             var logs = new List<NightlyProcessLog>
             {
                 new NightlyProcessLog
@@ -197,11 +222,16 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
                 }
             };
 
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
 
             _mockGateway
                 .Setup(x => x.GetByDateCreatedAsync(createdDate))
                 .ReturnsAsync(logs);
+
+            var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
 
             // Act
             var result = await _nightlyProcessLogUseCase.ExecuteAsync(createdDate).ConfigureAwait(false);
@@ -218,10 +248,16 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             // Arrange
             var createdDate = DateTime.UtcNow;
             var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
+
             _mockGateway
                 .Setup(x => x.GetByDateCreatedAsync(createdDate))
                 .Throws(new DbUpdateException("Database error"));
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
+
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
+
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
 
             // Act & Assert
             await Assert.ThrowsAsync<System.InvalidOperationException>(() => _nightlyProcessLogUseCase.ExecuteAsync(createdDate)).ConfigureAwait(false);
@@ -233,11 +269,15 @@ namespace HousingFinanceInterimApi.Tests.V1.UseCase
             // Arrange
             var createdDate = DateTime.UtcNow;
             var logGroups = new List<string> { "/aws/lambda/log-group-function1" };
-            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, logGroups);
+            _nightlyProcessLogUseCase = new NightlyProcessLogUseCase(_mockGateway.Object, _mockCloudWatchLogsClient.Object, _mockProvider.Object);
 
             _mockGateway
                 .Setup(x => x.GetByDateCreatedAsync(createdDate))
                 .Throws(new Exception("Unexpected error"));
+
+            _mockProvider
+                .Setup(x => x.GetLogGroups(It.IsAny<string>()))
+                .Returns(logGroups);
 
             // Act & Assert
             await Assert.ThrowsAsync<ApplicationException>(() => _nightlyProcessLogUseCase.ExecuteAsync(createdDate)).ConfigureAwait(false);
