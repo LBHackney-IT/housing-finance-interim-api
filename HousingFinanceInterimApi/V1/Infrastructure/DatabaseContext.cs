8fc7457efed70226777b8d2e418d3b7fdbc0e849
Adam <adam.tracy20@gmail.com>
2026-09-17 12:05:12 +0100

    Update all address lines in update asset details

--- HousingFinanceInterimApi/V1/Infrastructure/DatabaseContext.cs
+++ HousingFinanceInterimApi/V1/Infrastructure/DatabaseContext.cs
@@ -173,9 +173,16 @@ namespace HousingFinanceInterimApi.V1.Infrastructure
         /// </summary>
         private DbSet<Payment> PaymentsValue { get; set; }
 
-        public async Task UpdateAssetDetails(string propertyReference, string postPreamble, string fullAddress)
+        public async Task UpdateAssetDetails(
+            string propertyReference,
+            string postPreamble,
+            string addressLine1,
+            string addressLine2,
+            string addressLine3,
+            string addressLine4,
+            string postCode)
         {
-            await PerformInterpolatedTransaction($"usp_UpdateAssetDetails {propertyReference}, {postPreamble}, {fullAddress}")
+            await PerformInterpolatedTransaction($"usp_UpdateAssetDetails {propertyReference}, {postPreamble}, {addressLine1}, {addressLine2}, {addressLine3}, {addressLine4}, {postCode}")
                 .ConfigureAwait(false);
         }
 
