c4664ec8d9f026b9cb3eb683d15407a491efcfc7
Adam <74552077+adamtry@users.noreply.github.com>
2026-09-17 16:25:23 +0100

    Update all address lines in update asset details (#269)

--- HousingFinanceInterimApi/V1/Gateways/AssetGateway.cs
+++ HousingFinanceInterimApi/V1/Gateways/AssetGateway.cs
@@ -16,8 +16,15 @@ namespace HousingFinanceInterimApi.V1.Gateways
 
         public async Task UpdateAssetDetails(UpdateAssetDetailsQuery query, UpdateAssetDetailsRequest request)
         {
-            var fullAddress = $"{request.AddressLine1}, {request.AddressLine2}, {request.AddressLine3}, {request.AddressLine4}";
-            await _context.UpdateAssetDetails(query.PropertyReference, request.PostPreamble, fullAddress).ConfigureAwait(false);
+            await _context.UpdateAssetDetails(
+                query.PropertyReference,
+                request.PostPreamble,
+                request.AddressLine1,
+                request.AddressLine2,
+                request.AddressLine3,
+                request.AddressLine4,
+                request.PostCode
+            ).ConfigureAwait(false);
         }
     }
 }
--- HousingFinanceInterimApi/V1/Infrastructure/IDatabaseContext.cs
+++ HousingFinanceInterimApi/V1/Infrastructure/IDatabaseContext.cs
@@ -11,7 +11,14 @@ namespace HousingFinanceInterimApi.V1.Infrastructure
     {
         Task<IList<OperatingBalance>> GetOperatingBalancesAsync(DateTime? startDate, DateTime? endDate, int startWeek, int startYear, int endWeek, int endYear);
         Task<IList<Payment>> GetPaymentsAsync(string tenancyAgreementRef, string rentAccount, string householdRef, int count, string order);
-        Task UpdateAssetDetails(string propertyReference, string postPreamble, string addressLine1);
+        Task UpdateAssetDetails(
+            string propertyReference,
+            string postPreamble,
+            string addressLine1,
+            string addressLine2,
+            string addressLine3,
+            string addressLine4,
+            string postCode);
         Task<IList<Tenancy>> GetTenanciesAsync(string tenancyAgreementRef, string rentAccount, string householdRef);
         Task<IList<TenancyTransaction>> GetTenancyTransactionsAsync(string tenancyAgreementRef, string rentAccount, string householdRef, int count, string order);
         Task<IList<TenancyTransaction>> GetTenancyTransactionsByDateAsync(string tenancyAgreementRef, string rentAccount, string householdRef, DateTime startDate, DateTime endDate);
